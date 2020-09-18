d0d3011adc7abd552c0f2a6b3322f1ad3146037d
Andrew <ansinelnikov@gmail.com>
2020-09-18 10:11:27 +0300

    fixing some warnings

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -14,7 +14,7 @@ import {
 // Get current users profile
 export const getCurrentProfile = () => async (dispatch) => {
   try {
-    const res = await axios.get("/api/profile/me");
+    await axios.get("/api/profile/me");
 
     dispatch({
       type: GET_PROFILE,
